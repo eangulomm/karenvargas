08718237a2ccf44bab5dc6f75166ecc336f2c78a
e <e@e>
2026-07-20 15:24:36 -0500

    Conectar despliegue de cotizaciones

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.ATELIER_CONFIG = {
   // Pega aquí la URL /exec de tu despliegue de Google Apps Script.
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyAI1K3Q8-45VXmfbYzlJvN-HB3Ic3I0MDlC2JdtS8h_v25YzKbrmFkdvp7iOmU8PY9Fg/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwNkqXMqKg2mBfyqpkC1gvwA25f6p5x5KkJi6KL1SaXju4csJ2PV4S-K8Z1UAN5H_68YQ/exec",
 
   // Si la URL está vacía, la app usa datos locales para probar todo el CRUD.
   USE_DEMO_DATA_WHEN_EMPTY: true,
@@ -10,7 +10,7 @@ window.ATELIER_CONFIG = {
   LOCALE: "es-CO",
   CURRENCY: "COP",
   REQUEST_TIMEOUT_MS: 18000,
-  CACHE_KEY: "atelierStudio.cache.v1",
+  CACHE_KEY: "atelierStudio.cache.v2",
   DEMO_STORAGE_KEY: "atelierStudio.demo.v1",
 
   MAX_CLIENT_CARDS: 120,
